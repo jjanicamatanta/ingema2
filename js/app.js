6bc48e6e94e84b2d383999819e2d656332723565
Jose Anicama <j.j.anicamatanta@gmail.com>
2019-09-02 02:41:47 -0500

    update

--- js/app.js
+++ js/app.js
@@ -5,15 +5,33 @@
       document.title = title+" | Ingema";
     } 
 
-    function redirect(context,pageName,title){
+    function redirect(app,context,pageName,title){
         setTitle(title);
-        context.app.swap('');
-        context.render(pageName, {})
-          .appendTo(context.$element());
+        
+        var htmlFolderName = "html"; 
+        var templateFolderTemplate = htmlFolderName+"/templates";
+        var mainTemplate = "page.ms";
+        
+        if(pageName == "home.ms")
+          mainTemplate = "home.ms";
+        
+        app.load(htmlFolderName+'/'+pageName)
+            .then(function(partial) {
+              context.partials = {page: partial};
+              context.title = title;
+              // context.friend = context.params.friend;
+              context.partial(templateFolderTemplate+'/'+mainTemplate);  
+            });
+        // context.app.swap('');
+        // context.render(pageName, {})
+        //   .appendTo(context.$element());
     }
 
     var app = $.sammy('#app', function() {
-      
+
+       // include the plugin and alias mustache() to ms()
+      this.use('Mustache', 'ms');
+ 
       // this.use('Template');
       // this.around(function(callback) {
       //   var context = this;
@@ -25,25 +43,26 @@
       // });
   
       this.get(hash+'/', function(context) {
-        redirect(context,'home.html','Hola');
+        redirect(this,context,'home.ms','Hola');
       });
 
       this.get(hash+'/nuestros-servicios', function(context) {
-        redirect(context,'services.html','Nuestros Servicios');
+        redirect(this,context,'services.ms','Nuestros Servicios');
       });
 
       this.get(hash+'/acerca-de-ingema', function(context) {
-        redirect(context,'about.html','Acerca de Ingema');
+        redirect(this,context,'about.ms','Acerca de Ingema');
       });
 
       this.get(hash+'/contacto', function(context) {
-          redirect(context,'contact.html','Contáctanos');
+        redirect(this,context,'contact.ms','Contáctanos');        
       });
       this.get(hash+'/nuestro-equipo', function(context) {
-        redirect(context,'team.html','Nuestro Equipo');
+        redirect(this,context,'team.ms','Nuestro Equipo');        
       });
+
       this.get(hash+'/nuestro-portafolio', function(context) {
-        redirect(context,'portfolio.html','Nuestro Potafolio');
+        redirect(this,context,'portfolio.ms','Nuestro Potafolio');
       });
 
 
@@ -63,14 +82,14 @@
   
   
       this.before(function() {
-        $('footer, #app').hide();
+        // $('footer, #app').hide();
         var hash = document.location.hash;
         $("nav").find("li").removeClass("menu-active");
         $("nav").find("a[href='"+hash+"']").parent().addClass("menu-active");
       });
       
       this.after(function() {
-        $("#app, footer").fadeIn('slow');
+        // $("#app, footer").fadeIn('slow');
         // $('#preloader').show();
       });
   
